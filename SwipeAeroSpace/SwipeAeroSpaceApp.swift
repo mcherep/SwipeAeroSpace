b63888fa5cf985648823b6256249b8e00982127f
Tricster <mediosrity@gmail.com>
2025-01-25 17:27:25 +0800

    Initial Commit

---
+++ SwipeAeroSpace/SwipeAeroSpaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwipeAeroSpaceApp.swift
+//  SwipeAeroSpace
+//
+//  Created by Tricster on 1/25/25.
+//
+
+import SwiftUI
+
+@main
+struct SwipeAeroSpaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
